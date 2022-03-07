2cee0f34d92425e0a61a3db5318a368630911059
mahmoudkarem511 <97557149+mahmoudkarem511@users.noreply.github.com>
2022-03-07 21:13:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $(".loader").fadeOut('slow');
+});
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
